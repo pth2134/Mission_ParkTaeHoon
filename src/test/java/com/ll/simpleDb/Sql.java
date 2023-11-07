3648f2a1e4571e75a0c36fd4c296da789b12e7b7
taehoon <xogns2134@gmail.com>
2023-11-07 17:35:27 +0900

    11.3

--- src/test/java/com/ll/simpleDb/Sql.java
+++ src/test/java/com/ll/simpleDb/Sql.java
@@ -5,92 +5,92 @@ import java.util.*;
 
 public class Sql {
     private String query = "";
-    public Sql appendIn(String query, List ids){
+
+    public Sql appendIn(String query, List ids) {
         ArrayList<Object> list = (ArrayList<Object>) ids;
-        String range = "(`"+list.get(0)+"`";
+        String range = "(`" + list.get(0) + "`";
         for (int i = 1; i < list.size(); i++) {
-            range += ",`"+list.get(i)+"`";
+            range += ",`" + list.get(i) + "`";
         }
         range += ")";
         System.out.println(query);
         return this;
     }
 
-    public Sql append(String str) {
+    public Sql append(String query) {
+        this.query += query;
         System.out.println("not yet");
         return this;
     }
 
-    public Sql append(String title, String str) {
+    public Sql append(String query, String content) {
+        this.query += query.replace("= ?", "= " + content);
         System.out.println("not yet");
         return this;
     }
 
-    public Sql append(String str, int a) {
-        System.out.println("not yet");
-        return this;
-    }
-
-    public Sql append(String str, int a, int b) {
-        System.out.println("not yet");
-        return this;
-    }
-    public Sql append(String str, int a, int b, int c) {
-        System.out.println("not yet");
-        return this;
-    }
-
-    public Sql append(String str, int a, int b, int c, int d) {
+    public Sql append(String str, Object... Id) {
+        String Ids = Arrays.toString(Id)
+                .replace("(", "(`")
+                .replaceAll(", ", ",`")
+                .replace(")", "`)");
+        this.query += Ids;
         System.out.println("not yet");
         return this;
     }
 
     public long insert() {
+        //query문을 입력하고 table에 insert하고 Id를 받아옴
+        this.query = "";
         System.out.println("not yet");
         return 1;
     }
 
     public long update() {
+        //업데이트된 row 갯수 return
+        this.query = "";
         System.out.println("not yet");
         return 1;
     }
 
     public long delete() {
+        //삭제된 row 갯수 return
+        this.query = "";
         System.out.println("not yet");
         return 1;
     }
 
-    public LocalDateTime selectDatetime(){
+    public LocalDateTime selectDatetime() {
         System.out.println("not yet");
         return LocalDateTime.now();
     }
 
-    public long selectLong(){
+    public long selectLong() {
         System.out.println("not yet");
         return 1;
     }
 
-    public ArrayList<Long> selectLongs(){
+    public ArrayList<Long> selectLongs() {
         System.out.println("not yet");
         return new ArrayList<>();
     }
 
-    public String selectString(){
+    public String selectString() {
         System.out.println("not yet");
         return "";
     }
 
-    public Map selectRow(){
+    public Map selectRow() {
         System.out.println("not yet");
         return new HashMap();
     }
 
-    public Article selectRow(Class<Article> clas){
+    public Article selectRow(Class<Article> clas) {
         System.out.println("not yet");
         return new Article();
     }
 
-    public ArrayList<Article> selectRows(Class<Article> clas){
+    public ArrayList<Article> selectRows(Class<Article> clas) {
         System.out.println("not yet");
         return new ArrayList<>();
     }
