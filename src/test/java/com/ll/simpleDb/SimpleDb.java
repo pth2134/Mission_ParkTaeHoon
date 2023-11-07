4550f4b7fb3fab317f94cdc729ad5a104de00470
taehoon <xogns2134@gmail.com>
2023-11-07 16:46:08 +0900

    11.모양만

--- src/test/java/com/ll/simpleDb/SimpleDb.java
+++ src/test/java/com/ll/simpleDb/SimpleDb.java
@@ -12,22 +12,25 @@ public class SimpleDb {
     String simpleDb__test;
     boolean devMode;
 
+
     SimpleDb(String localhost,String root, String blank, String simpleDb_test){
         this.localhost = localhost;
         this.root = root;
         this.blank = blank;
         this.simpleDb__test = simpleDb_test;
+        System.out.println("not yet");
     }
 
     public void run(String query){
-
+        System.out.println("not yet");
     }
 
     public void run(String query, String title, String body, boolean isBlind){
-
+        System.out.println("not yet");
     }
 
     public Sql genSql() {
+        System.out.println("not yet");
         return new Sql();
     };
 
--- src/test/java/com/ll/simpleDb/Sql.java
+++ src/test/java/com/ll/simpleDb/Sql.java
@@ -9,69 +9,86 @@ import java.util.Map;
 public class Sql {
 
     public Sql appendIn(String str, List ids){
+        System.out.println("not yet");
         return new Sql();
     }
 
     public Sql append(String str) {
+        System.out.println("not yet");
         return new Sql();
     }
 
     public Sql append(String title, String str) {
+        System.out.println("not yet");
         return new Sql();
     }
 
     public Sql append(String str, int a) {
+        System.out.println("not yet");
         return new Sql();
     }
 
     public Sql append(String str, int a, int b) {
+        System.out.println("not yet");
         return new Sql();
     }
     public Sql append(String str, int a, int b, int c) {
+        System.out.println("not yet");
         return new Sql();
     }
 
     public Sql append(String str, int a, int b, int c, int d) {
+        System.out.println("not yet");
         return new Sql();
     }
 
     public long insert() {
+        System.out.println("not yet");
         return 1;
     }
 
     public long update() {
+        System.out.println("not yet");
         return 1;
     }
 
     public long delete() {
+        System.out.println("not yet");
         return 1;
     }
 
     public LocalDateTime selectDatetime(){
+        System.out.println("not yet");
         return LocalDateTime.now();
     }
 
     public long selectLong(){
+        System.out.println("not yet");
         return 1;
     }
 
     public ArrayList<Long> selectLongs(){
+        System.out.println("not yet");
         return new ArrayList<>();
     }
 
     public String selectString(){
+        System.out.println("not yet");
         return "";
     }
 
     public Map selectRow(){
+        System.out.println("not yet");
         return new HashMap();
     }
 
     public Article selectRow(Class<Article> clas){
+        System.out.println("not yet");
         return new Article();
     }
 
     public ArrayList<Article> selectRows(Class<Article> clas){
+        System.out.println("not yet");
         return new ArrayList<>();
     }
 
